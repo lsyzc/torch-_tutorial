4e5643f1ce2901f90b639b7ebe42d2957c7c5194
lishengyao <li-shengyao@foxmail.com>
2025-01-13 21:29:30 +0800

    go bed1

--- tensor.py
+++ tensor.py
@@ -37,29 +37,29 @@ print(tensor.half())
 
 import numpy as np
 
-x = np.zeros((3,3))
+x = np.zeros((3, 3))
 x = torch.from_numpy(x)
 x_back = x.numpy()
 print(x_back)
 
-#Addition
-x = torch.tensor([1,2,3])
-y = torch.tensor([4,5,6])
+# Addition
+x = torch.tensor([1, 2, 3])
+y = torch.tensor([4, 5, 6])
 print(x + y)
-z = torch.add(x,y)
+z = torch.add(x, y)
 z1 = torch.empty(3)
 print(z1)
-torch.add(x,y,out=z1)
+torch.add(x, y, out=z1)
 print(z1)
 # subtraction
 z = x - y
 # Division
-z = torch.true_divide(x,y)
+z = torch.true_divide(x, y)
 # inplace operation
 t = torch.zeros(3)
 t.add_(x)
-t += x # inplace
-#t = t + x # create a new one
+t += x  # inplace
+# t = t + x # create a new one
 print(t)
 # exponentiation
 z = x.pow(2)
@@ -68,23 +68,97 @@ z = x**2
 z = x > 0
 print(z)
 # matrix mutiplication
-x1 = torch.rand((2,5))
-x2 = torch.rand((5,2))
+x1 = torch.rand((2, 5))
+x2 = torch.rand((5, 2))
 print(x1.shape)
-z2 = torch.mm(x1,x2)#z2 = torch.mul(x1,x2) 注释的是按元素相乘，会广播
+z2 = torch.mm(x1, x2)  # z2 = torch.mul(x1,x2) 注释的是按元素相乘，会广播
 
 z3 = x1.mm(x2)
 print(x1)
 print(x2)
 # matrix exponentiation
-matrix_one = torch.rand((5,5))
+matrix_one = torch.rand((5, 5))
 mmatrix_power = matrix_one.matrix_power(3)
 print(mmatrix_power)
 
-#x = torch.rand((3,5,5))
-x = torch.arange(75).reshape((3,5,5))
+# x = torch.rand((3,5,5))
+x = torch.arange(75).reshape((3, 5, 5))
 print(x.size())
-y = torch.sum(x,dim=0)
+y = torch.sum(x, dim=0)
 print(y.shape)
 print(x)
 print(y)
+x = torch.arange(5)
+print(x)
+
+print(type(torch.max(x, dim=0)))
+max_item, index = torch.max(x, dim=0)
+print(type(max_item))
+item = torch.max(x, dim=0)
+print(item)
+x = torch.arange(5)
+print(x)
+print(
+    type(torch.max(x, dim=0))
+)  # torch.max / torch.min只要制定了dim 返回的结果就是一个tuple 包含values 和 indices
+values, index_t = torch.max(x, dim=0)
+print(values)
+print(index_t)
+print(torch.argmax(x))
+m = torch.mean(x.float())  # the input tensor, either of floating point or complex dtype
+print(m)
+y = torch.arange(1, 6)
+y[0] = 0
+print(type(y[0]))
+print(x == y)
+print(torch.equal(x, y))
+x = torch.tensor([12, 10, 2, 1])
+sorted_x = torch.sort(x, dim=0)
+print(type(sorted_x))
+y, i = sorted_x
+print(type(y))
+print(y)
+print(i)
+z = torch.clamp(
+    x, min=0, max=10
+)  # any number in blow min will be set to min and number above max will be set to 10
+print(z)
+
+x = torch.tensor([0, 1, 1, 0, 1])
+z = torch.any(x, keepdim=True)  # 其中有一个为真就是真
+print(z)
+z2 = torch.all(x, keepdim=True)  # keep dim 输入与输出一个维度
+print(z2)
+
+# tensor indexing
+batch_size = 10
+features = 25
+x = torch.rand((batch_size, features))
+print(x.shape)
+print(x[0].shape)
+print(x[:, 0].shape)
+print(x[:, 0].unsqueeze(dim=1).shape)
+x[0, 0] = 100
+print(x[0, 0] == 100)
+
+# fancy indexing
+x = torch.arange(10)
+indices = [2, 4, 5]
+print(x[indices])
+
+x = torch.arange(9).reshape((3,3))
+print(x)
+rows = [0,1]
+cols = [1,2]
+print(x[rows,cols]) #返回的是x[0,1] and cols[1,2] 易错
+# more advanced indexing
+x = torch.arange(10)
+print(x)
+print(x[(x<2) | (x>8)])
+
+print(x.remainder(2) == 0) # x.remainder(2) 是x模2
+
+#other usefule oprations
+print(torch.where(x > 5,x,x*2)) # x > 5 ? x:2*x
+print(torch.tensor([0,0,1,1,2,4]).unique())
+
